35c2ec5e19ce34c140b666428a99fb451d6c9f2c
RemiRemiGit <RODRIGOMONTES@ME.COM>
2026-07-27 21:59:46 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'planificador-v2.5';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache).catch(() => {
+          console.log('Algunos recursos no pudieron ser cacheados');
+        });
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activar Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Interceptar requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+      .catch(() => {
+        return new Response('Offline - sin conexión disponible');
+      })
+  );
+});
